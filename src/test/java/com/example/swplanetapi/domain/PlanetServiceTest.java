8c795661ae6a29ef45d9bbea3361a9d30646c0f2
Akssasori <diniz3003@gmail.com>
2023-07-15 07:37:22 -0300

    test list all planets

--- src/test/java/com/example/swplanetapi/domain/PlanetServiceTest.java
+++ src/test/java/com/example/swplanetapi/domain/PlanetServiceTest.java
@@ -9,13 +9,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Example;
 
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Predicate;
 
 import static com.example.swplanetapi.common.PlanetConstants.*;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 
@@ -51,15 +51,63 @@ public class PlanetServiceTest {
     public void getPlanet_ByExistingId_ReturnsPlanet() {
 
         when(planetRepository.findById(anyLong())).thenReturn(Optional.of(PLANET));
-        Optional<Planet> planet = planetService.get(anyLong());
+        Optional<Planet> planet = planetService.get(1L);
+
+        Assertions.assertThat(planet).isNotEmpty();
         Assertions.assertThat(planet.get()).isEqualTo(PLANET);
 
     }
     @Test
     public void getPlanet_ByUnexistingId_ReturnsPlanet() {
         when(planetRepository.findById(anyLong())).thenReturn(Optional.empty());
-        Optional<Planet> planet = planetService.get(anyLong());
+        Optional<Planet> planet = planetService.get(1L);
+
+        Assertions.assertThat(planet).isEmpty();
         Assertions.assertThat(planet).isEqualTo(OPTIONAL_EMPYTY_PLANET);
     }
+
+    @Test
+    public void getPlanet_ByExistingName_ReturnsPlanet() {
+
+        when(planetRepository.findByName(anyString())).thenReturn(Optional.of(PLANET));
+        Optional<Planet> planet = planetService.getByName("tatooine");
+
+        Assertions.assertThat(planet).isNotEmpty();
+        Assertions.assertThat(planet.get()).isEqualTo(PLANET);
+    }
+    @Test
+    public void getPlanet_ByUnexistingName_ReturnsPlanet() {
+
+        when(planetRepository.findByName(anyString())).thenReturn(Optional.empty());
+        Optional<Planet> planet = planetService.getByName("Unexisting name");
+
+        Assertions.assertThat(planet).isEmpty();
+        Assertions.assertThat(planet).isEqualTo(OPTIONAL_EMPYTY_PLANET);
+
+    }
+    @Test
+    public void listPlanets_ReturnsAllPlanets() {
+
+        Example<Planet> query = QueryBuilder.makeQuery(new Planet(PLANET.getClimate(), PLANET.getTerrain()));
+        when(planetRepository.findAll(query)).thenReturn(List.of(PLANET));
+        List<Planet> list = planetService.list(PLANET.getTerrain(), PLANET.getClimate());
+
+        Assertions.assertThat(list).isNotEmpty();
+        Assertions.assertThat(list).hasSize(1);
+        Assertions.assertThat(list.get(0)).isEqualTo(PLANET);
+
+    }
+    @Test
+    public void listPlanets_ReturnsNoPlanets() {
+
+        // List<Planet> planets = new ArrayList<>(); << poderia usar uma lista vazia
+        when(planetRepository.findAll(any())).thenReturn(Collections.emptyList());
+        List<Planet> list = planetService.list(PLANET.getTerrain(), PLANET.getClimate());
+
+        Assertions.assertThat(list).isEmpty();
+
+
+    }
+
 
 }
