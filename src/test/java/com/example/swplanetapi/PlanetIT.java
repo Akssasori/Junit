1878e3cfdacd9617c6d63cde68a63020547618c4
Akssasori <diniz3003@gmail.com>
2023-07-18 21:04:05 -0300

    test sugvutaneos

---
+++ src/test/java/com/example/swplanetapi/PlanetIT.java
@@ -0,0 +1,71 @@
+package com.example.swplanetapi;
+
+import com.example.swplanetapi.domain.Planet;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+import static com.example.swplanetapi.common.PlanetConstants.TATOOINE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static com.example.swplanetapi.common.PlanetConstants.PLANET;
+
+
+@ActiveProfiles("it")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = { "/import_planets.sql" }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = { "/remove_planets.sql" }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class PlanetIT {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void createPlanet_ReturnsCreated() {
+        ResponseEntity<Planet> sut = restTemplate.postForEntity("/planets", PLANET, Planet.class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(sut.getBody().getId()).isNotNull();
+        assertThat(sut.getBody().getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getBody().getClimate()).isEqualTo(PLANET.getClimate());
+        assertThat(sut.getBody().getTerrain()).isEqualTo(PLANET.getTerrain());
+    }
+
+    @Test
+    public void getPlanet_ReturnsPlanet() {
+        ResponseEntity<Planet> sut = restTemplate.getForEntity("/planets/1", Planet.class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(sut.getBody()).isEqualTo(TATOOINE);
+    }
+    @Test
+    public void getPlanetByName_ReturnsPlanet() {
+        // TODO implement
+    }
+
+    @Test
+    public void listPlanets_ReturnsAllPlanets() {
+        // TODO implement
+    }
+
+    @Test
+    public void listPlanets_ByClimate_ReturnsPlanets() {
+        // TODO implement
+    }
+
+    @Test
+    public void listPlanets_ByTerrain_ReturnsPlanets() {
+        // TODO implement
+    }
+
+    @Test
+    public void removePlanet_ReturnsNoContent() {
+        // TODO implement
+    }
+
+}
